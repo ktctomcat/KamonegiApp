679ea1e47332c761c59a894c59c34eb4e26dc9da
Tomoo_Inspiron_14 <ktc.tomcat@gmail.com>
2025-04-20 17:54:52 +0900

    20250420

--- app/src/main/java/io/github/ktctomcat/android/kamonegiapps/WebcamActivity.kt
+++ app/src/main/java/io/github/ktctomcat/android/kamonegiapps/WebcamActivity.kt
@@ -6,20 +6,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class WebcamActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         //　システムバーの領域にもビューを広げるための設定。
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_webcam)
 
         // システムバー（ステータスバーやナビゲーションバー）によってビューが隠れないように調整する設定。
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.webcam)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
+
 
 
     }
